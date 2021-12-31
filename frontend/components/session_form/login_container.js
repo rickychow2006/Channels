d62592caa26e856c233b92ff94801df53a1ff568
rickychow2006 <rickychow0716@gmail.com>
2021-12-31 17:57:44 -0600

    completed login in redux

---
+++ frontend/components/session_form/login_container.js
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { login, clearSessionErrors } from "../../actions/session_actions";
+import LoginForm from "./login_form";
+import React from "react";
+
+const mapStateToProps = state => ({
+  user: {
+      email: "",
+      password: ""
+  },
+  errors: state.errors.sessionErrors
+});
+
+const mapDispatchToProps = dispatch => ({
+  clearSessionErrors: () => dispatch(clearSessionErrors()),
+  login: user => dispatch(login(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -5,13 +5,51 @@ class LoginForm extends React.Component {
 
   constructor(props) {
     super(props);
-  }
+    this.state = props.user;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.demoUserSubmit = this.demoUserSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+};
+
+  componentWillUnmount() {
+    this.props.clearSessionErrors();
+  };
+
+  update(field) {
+    return event => (
+      this.setState({ [field]: event.target.value})
+    )
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.login(this.state);
+  };
+
+  demoUserSubmit(event) {
+    event.preventDefault();
+    this.props.log({email:"demouser@gmail.com", password:"password"})
+  };
+
+  renderErrors() {
+    let { errors } = this.props;
+
+    if (errors.length === 0) {
+      return null;
+    }
+    
+    return (
+      <div className="login-errors">
+        {errors.join(" \u2022 ")}
+      </div>   
+    )
+  };
 
   render() {
     return (
       <div className="form-page">
         <div className="form__wrapper">
-          <form> 
+          <form onClick={this.handleSubmit} > 
             <div className="form__header">
               <h1>Sign in to Channels</h1>
             </div>
@@ -19,31 +57,31 @@ class LoginForm extends React.Component {
                 Enter your email address and password.
               </h3>
             <div className="errors">
-
+              {this.renderErrors()}
             </div>
             <div className="form__input">
               <input required
                   type="email"
-                  placeholder="Please Type Your Email..."
-              >
-              </input>
+                  value={this.state.email}
+                  onChange={this.update('email')}
+                  placeholder="Please Type Your Email..." />
             </div>
             <div className="form__input">
               <input required
                   type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
                   minLength="6"
-                  placeholder="Input Password"
-              >
-              </input>
+                  placeholder="Input Password"/>
             </div>
             <div className='form__button'>
-              <button type="submit">Sign Up</button>
+              <button type="submit">Login Up</button>
             </div>
           </form>
         </div>
       </div>
     )
   }
-}
+};
 
 export default LoginForm;
