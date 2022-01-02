bc49af481c5b80175cfa07ca5eb93df911019713
rickychow2006 <rickychow0716@gmail.com>
2022-01-02 17:33:42 -0600

    added entities reducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import errorsReducer from './errors_reducer';
+import sessionReducer from './session.reducer';
+import entitiesReducer from './entities_reducer';
+
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  errors: errorsReducer,
+  entities: entitiesReducer
+})
+
+export default rootReducer;
