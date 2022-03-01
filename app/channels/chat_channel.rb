62e302ac03aef264e0913ec1cfdfeb64315bea05
rickychow2006 <rickychow0716@gmail.com>
2022-03-01 16:21:38 -0600

    set up web socket

---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,37 @@
+class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    if(params[:channelId])
+      @channel = (params[:channelId])
+    elsif (params[:dmId])
+      @dmchannel = params[:dmId]
+    end 
+    stream_for @channel || @dmchannel
+  end
+
+  def speak(data) 
+    if data['channel_id']
+      message = Cmessage.new(body: data['body'], author_id: data['author_id'], channel_id: data['channel_id'])
+      if message.save
+        socket = {message: message}
+        ChatChannel.broadcast_to(data['channel_id'], socket)
+      end 
+    elsif data['receiver_id']
+      
+      actual_receiver = data['receiver_id'].clone 
+      author_id = data ['author_id']
+      actual_receiver.slice!(author_id.to_s)
+
+      message = Dmessage.new(body: data['body'], author_id: data['author_id'], receiver_id: actual_receiver)
+
+      if message.save 
+        socket = {message: message}
+        ChatChannel.broadcast_to(data['receiver_id'], socket)
+      end 
+    end 
+  end 
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
