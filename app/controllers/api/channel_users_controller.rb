bd6e18d97419887fbd9e00602b5350a49fb96eba
rickychow2006 <rickychow0716@gmail.com>
2022-03-01 15:34:51 -0600

    completed controller for channel users

---
+++ app/controllers/api/channel_users_controller.rb
@@ -0,0 +1,41 @@
+class Api::ChannelUsersController < ApplicationController
+
+  def index 
+    if params[:channelId]
+      @channelusers = ChannelUser.where(channel_id: params[:channelId])
+      render 'api/channel_users/index' 
+    else 
+      render json: ["Invalid action"], status: 401
+    end 
+  end 
+
+  def create 
+    @channeluser = ChannelUser.new(channeluser_params)
+    
+    if @channeluser.save
+    else 
+      render json:["Invalid action"], status 401
+    end
+  end 
+
+  def update 
+    @channeluser = ChannelUser.find_by(user_id: params[:channeluser][:user_id], channel_id: params[:channeluser][:channel_id])
+    @channeluser.update(channeluser_params)
+  end 
+
+  def destroy 
+    @channeluser = ChannelUser.find_by(user_id: params[:channeluser][:user_id], channel_id: params[:channeluser][:channel_id])
+
+    if @channeluser
+      @channeluser.destroy
+    else 
+      render json: ['sorry, only admin of the channel has access to delete the channel'], status: 404
+    end 
+  end 
+
+  private 
+  def channeluser_params 
+    params.require(:channeluser).permit(:user_id, :channel_id, :pending)
+  end 
+
+end
