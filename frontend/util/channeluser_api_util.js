a411f141e2387ee050e1d5dceabada8fe1cc4bc5
rickychow2006 <rickychow0716@gmail.com>
2022-03-05 12:43:58 -0600

    ajax fetch for channel and channel_user

---
+++ frontend/util/channeluser_api_util.js
@@ -0,0 +1,42 @@
+export const fetchChannelUsers = (channelId) =>{
+  return $.ajax({
+    url:`/api/channel_users`,
+    method:"GET",
+    data: {channelId:channelId}
+  })
+}
+
+export const createChannelUser = (channelId, userId, pending) =>{
+  return $.ajax({
+    url:`/api/channel_users`,
+    method:"POST",
+    data: {channeluser:{
+      user_id:userId,
+      channel_id:channelId,
+      pending: pending
+    }}
+  })
+}
+
+export const patchChannelUser = (channelId, userId, pending) =>{
+  return $.ajax({
+    url:`/api/channel_users/${userId}`,
+    method:"PATCH",
+    data: {channeluser:{
+      user_id:userId,
+      channel_id:channelId,
+      pending: pending
+    }}
+  })
+}
+
+export const deleteChannelUser = (channelId, userId) =>{
+  return $.ajax({
+    url:`/api/channel_users/${userId}`,
+    method:"DELETE",
+    data: {channeluser:{
+      user_id:userId,
+      channel_id:channelId
+    }}
+  })
+}
