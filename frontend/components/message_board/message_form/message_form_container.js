dab3a5f2556af1043e234d45500b6cdc882a2f28
rickychow2006 <rickychow0716@gmail.com>
2022-03-06 12:53:07 -0600

    message form container

---
+++ frontend/components/message_board/message_form/message_form_container.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import MessageFrom from './message_form';
+import { withRouter } from "react-router";
+import {createCMessage} from '../../../actions/cmessage_actions';
+import {createDMessage} from '../../../actions/dmessage_actions';
+import {createChannelUser} from '../../../actions/channeluser_actions';
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    currentUser: state.session.currentUser,
+    currentChannel: state.entities.allchannels[ownProps.match.params.channelId],
+    currentDM:ownProps.match.params.dmId,
+    allchannels: state.entities.allchannels,
+    channels: state.entities.channels,
+    channelUsers: state.entities.channelusers
+}};
+
+const mapDispatchToProps = dispatch => ({
+  createCMessage: (cmessage) => dispatch(createCMessage(cmessage)), 
+  createDMessage: (message) => dispatch(createDMessage(message)), 
+  createChannelUser: (channelId, userId, pending) => dispatch(createChannelUser(channelId, userId, pending)),
+});
+
+export default  withRouter(connect(mapStateToProps,mapDispatchToProps)(MessageFrom));
