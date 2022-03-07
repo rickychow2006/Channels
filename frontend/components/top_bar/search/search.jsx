5261ad36221398af3bb64510e26e89761d448b4e
rickychow2006 <rickychow0716@gmail.com>
2022-03-07 12:41:31 -0600

    search bar

--- frontend/components/top_bar/search/search.jsx
+++ frontend/components/top_bar/search/search.jsx
@@ -4,16 +4,37 @@ import { MdSearch } from 'react-icons/md';
 class Search extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      inputValue: '',
+      showSearch: 'search-li-noshow'
+    };
+    this.handleFocuse = this.handleFocuse.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+  }
+  handleFocuse(event) {
+    event.preventDefault();
+    if (this.state.showSearch === 'search-li-noshow'){
+      this.setState({showSearch:'search-li'})}
+  }
+  
+  handleClose(event) {
+    event.preventDefault();
+    if (this.state.showSearch === 'search-li'){
+        this.setState({showSearch:'search-li-noshow'})
+      }
   }
 
   render () {
+
     return ( 
-      <div className='search'>
+      <div className='search' onFocus={this.handleFocuse} onBlur={this.handleClose}>
         <MdSearch className='search-icons' size='1.3em' />
         <input 
         className='search-input'
         placeholder='Search ...'
         />
+        <ul className={this.state.showSearch} >"hi"</ul>
     </div>
     )
   }
---
+++ frontend/components/top_bar/search/search_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { withRouter } from "react-router";
+import Search from './search';
+import {fetchAllChannels} from '../../../actions/channel_actions'
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.session.currentUser,
+    allchannelsState: state.entities.allchannels,
+    allchannels: Object.values(state.entities.allchannels),
+    users: Object.values(state.entities.users)
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  fetchAllChannels: () => dispatch(fetchAllChannels())
+});
+
+export default withRouter(connect( mapStateToProps, mapDispatchToProps)(Search));
